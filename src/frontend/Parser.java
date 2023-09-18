210799c144edb047891656cead9a3aac788a2232
Victoria Le <95896163+levictoria0117@users.noreply.github.com>
2023-09-17 19:29:55 -0700

    Update Parser.java

--- src/frontend/Parser.java
+++ src/frontend/Parser.java
@@ -128,6 +128,7 @@ public class Parser
             case WRITE :      stmtNode = parseWriteStatement();      break;
             case WRITELN :    stmtNode = parseWritelnStatement();    break;
             case IF:          stmtNode = parseIfStatement(); break;
+            case FOR :      stmtNode = parseForStatement();      break;
             case SEMICOLON :  stmtNode = null; break;  // empty statement
             
             default : syntaxError("Unexpected token at parseStatement");
